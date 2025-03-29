d82e0a75bef206619a213609eb8438a161943ea1
Bernardo Gama <76955720+Beny-Gama@users.noreply.github.com>
2025-03-29 00:54:08 -0400

    fazendo mais um poco

--- CeV/ex071.py
+++ CeV/ex071.py
@@ -32,15 +32,10 @@ def whileS():
             cont1 += 1
             valor -= 1
 
-    if cont100 != 0: print(f'Total de {cont100} de edulas de R$:100.00')
-    if cont50  != 0:print(f'Total de {cont50} de edulas de R$:50.00')
-    if cont20  != 0:print(f'Total de {cont20} de cedulas de R$:20.00')
-    if cont10  != 0:print(f'Total de {cont10} de cedulas de R$:10.00')
-    if cont5   != 0:print(f'Total de {cont5} de celulas de R$:5.00')
-    if cont1   != 0:print(f'Total de {cont1} de celulas de R$:1.00')
+
+    
 
 print('The while solution'.center(40))
-whileS()
 
 
 def mathS(): # notação: O(1)
@@ -64,7 +59,7 @@ def mathS(): # notação: O(1)
     cont1 = valor // 1
     valor %= 1
 
-
+    
     if cont100 != 0: print(f'Total de {cont100} de cedulas de R$:100.00')
     if cont50  != 0: print(f'Total de {cont50} de cedulas de R$:50.00')
     if cont20  != 0: print(f'Total de {cont20} de cedulas de R$:20.00')
@@ -73,4 +68,33 @@ def mathS(): # notação: O(1)
     if cont1   != 0: print(f'Total de {cont1}  de celulas de R$:1.00')
 
 print('The math solution'.center(40))
+
+def CeV():
+    print('='*30)
+    print('{:30}'.format('banco CeV').upper())
+    print('='*30)
+
+    while True:
+        valor = int(input('Qual Valor para sacar? '))
+        total = valor
+        cedula = 50
+        totalCelulas = 0
+        if total >= cedula:
+            total -= cedula
+            totalCelulas += 1
+        else:
+            if totalCelulas > 0:
+                print(f'Total de {totalCelulas} celulas de R$:{cedula}')
+            if cedula == 50:
+                cedula = 20
+            elif cedula == 20:
+                cedula = 10
+            elif cedula == 10:
+                cedula = 1
+            if cedula == 0:
+                break
+
+whileS()
 mathS()
+
+
---
+++ CeV/ex072.py
@@ -0,0 +1,14 @@
+cont = (
+    'zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+    'seis', 'sete', 'oito', 'nove', 'dez',
+    'onze', 'doze', 'treze', 'quatorze', 'quinze',
+    'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte',
+)
+while True:
+    num = int(input('Digite um numero interio: '))
+    if 0 <= num <= 20:
+        break
+    
+    print('Tente novamente. ', end='')
+print(f'Você digitou o numero: {cont[num].upper()}')
+
